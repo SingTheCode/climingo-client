433efe26abd84917526fcd1bc3a2fc254fffe54d
gxxrxn <rbfks1221@naver.com>
2024-04-04 22:17:01 +0900

    [#24] chore: eslint recommended, custom rule 설정
    - commitlint config eslint(import/no-anonymous-default-export) 오류 해결

---
+++ .eslintrc.js
@@ -0,0 +1,24 @@
+module.exports = {
+  extends: [
+    "next/core-web-vitals",
+    "next",
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "prettier",
+  ],
+  plugins: ["prettier"],
+  rules: {
+    // typescript-eslint rule을 정의하기 위해 기존 규칙 off
+    "no-unused-vars": "off",
+    // 사용하지 않은 변수(인자)가 있으면 에러, _로 시작하는 변수(인자)는 무시
+    "@typescript-eslint/no-unused-vars": [
+      "error",
+      {
+        varsIgnorePattern: "^_",
+        args: "all",
+        argsIgnorePattern: "^_",
+        caughtErrors: "all",
+      },
+    ],
+  },
+};
