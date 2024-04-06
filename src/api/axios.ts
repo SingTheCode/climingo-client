e217d4d17fa265011ba10fca2bd40d6291576e0a
spiderq2697 <spiderq10@gmail.com>
2024-04-06 22:59:39 +0900

    [#12] feat: get, post, put, delete에 대한 API 모듈 생성

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -1,10 +1,43 @@
-import axios from "axios";
+import axios, { AxiosResponse } from "axios";
 
 export const api = axios.create({
   baseURL: `${process.env.NEXT_PUBLIC_API_URL}`,
 });
 
-// 응답 인터셉터
-api.interceptors.response.use((res) => {
-  return res;
-});
+export const API = {
+  get: async <T = any>(url: string, query = {}): Promise<AxiosResponse<T>> => {
+    query = new URLSearchParams(query);
+    return await api.get(`${url}${Object.keys(query) > 0 ? "?" + query : ""}`);
+  },
+  post: async <T = any>(
+    url: string,
+    query = {},
+    params?: any
+  ): Promise<AxiosResponse<T>> => {
+    query = new URLSearchParams(query);
+    return await api.post(
+      `${url}${Object.keys(query) > 0 ? "?" + query : ""}`,
+      params
+    );
+  },
+  put: async <T = any>(
+    url: string,
+    query = {},
+    params?: any
+  ): Promise<AxiosResponse<T>> => {
+    query = new URLSearchParams(query);
+    return await api.put(
+      `${url}${Object.keys(query) > 0 ? "?" + query : ""}`,
+      params
+    );
+  },
+  delete: async <T = any>(
+    url: string,
+    query = {}
+  ): Promise<AxiosResponse<T>> => {
+    query = new URLSearchParams(query);
+    return await api.delete(
+      `${url}${Object.keys(query) > 0 ? "?" + query : ""}`
+    );
+  },
+};
