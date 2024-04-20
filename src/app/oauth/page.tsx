de996e40ab95b29291ef69f8b56bb83e42a2547c
spiderq2697 <spiderq10@gmail.com>
2024-04-20 11:23:37 +0900

    [#11] refactor: OAuth 페이지에서 userQuery를 통해 api를 호출하도록 적용

--- src/app/oauth/page.tsx
+++ src/app/oauth/page.tsx
@@ -1,43 +1,5 @@
-"use client";
+import OAuth from "@/app/oauth/OAuth";
 
-import { useEffect } from "react";
-import { useRouter, useSearchParams } from "next/navigation";
-import { oAuthApi, signInApi } from "@/api/modules/user";
-
-export default function OAuth() {
-  const router = useRouter();
-  const code = useSearchParams().get("code");
-
-  const oAuth = async () => {
-    if (!code) {
-      return;
-    }
-
-    const params = {
-      providerType: "kakao",
-      redirectUri: window.location.origin + window.location.pathname,
-      code,
-    } as const;
-
-    const { isAlreadySignUp, provider, oAuthToken } = await oAuthApi(params);
-
-    if (isAlreadySignUp) {
-      // TODO: userInfo store 에 저장
-      await signInApi({
-        provider,
-        oAuthToken,
-      });
-
-      // TODO: 둘러보기 페이지로 이동
-      router.push("/");
-      return;
-    }
-    router.push("/signUp");
-  };
-
-  useEffect(() => {
-    oAuth();
-  }, []);
-
-  return <div></div>;
+export default function Page() {
+  return <OAuth />;
 }
