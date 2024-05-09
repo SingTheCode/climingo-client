f74d2977c0f34a64cfb1b5b1a91cf906cf52a273
gxxrxn <rbfks1221@naver.com>
2024-05-10 05:51:28 +0900

    [#13] feat: header에 home icon 추가

--- src/components/common/NavigationHeader.tsx
+++ src/components/common/NavigationHeader.tsx
@@ -1,27 +1,28 @@
 /* eslint-disable @next/next/no-img-element */
 "use client";
 
+import Link from "next/link";
 import { useRouter } from "next/navigation";
 
-type BackButtonType = "default" | "home";
 type NavigationHeaderProps = {
   pageTitle?: string;
   hideBackButton?: boolean;
-  backButtonType?: BackButtonType;
+  hideHomeButton?: boolean;
   rightElement?: React.ReactNode;
 };
 
 const NavigationHeader = ({
   pageTitle,
   hideBackButton = false,
-  backButtonType = "default",
+  hideHomeButton = false,
   rightElement,
 }: NavigationHeaderProps) => {
   return (
     <nav className="h-[4.8rem] fixed top-0 left-0 flex items-center justify-between w-screen z-30 overflow-y-hidden bg-white">
       {/** left */}
-      <div className="flex p-[0.2rem] basis-[10rem]">
-        {!hideBackButton && <BackButton backButtonType={backButtonType} />}
+      <div className="flex px-[0.2rem] basis-[10rem] gap-[0.4rem] items-center">
+        {!hideBackButton && <BackButton />}
+        {!hideHomeButton && <HomeButton />}
       </div>
 
       {/** center */}
@@ -30,7 +31,7 @@ const NavigationHeader = ({
       </h2>
 
       {/** right */}
-      <div className="flex basis-[10rem] justify-end pr-[1.5rem] py-[0.2rem]">
+      <div className="flex basis-[10rem] justify-end  py-[0.2rem] items-center gap-[0.4rem] pr-[1rem]">
         {rightElement}
       </div>
     </nav>
@@ -39,29 +40,29 @@ const NavigationHeader = ({
 
 export default NavigationHeader;
 
-const BackButton = ({ backButtonType }: { backButtonType: BackButtonType }) => {
+const BackButton = () => {
   const router = useRouter();
 
-  const goBack = () => router.back();
+  const goPrev = () => router.back();
 
   return (
-    <button className="p-[0.8rem]" onClick={goBack}>
-      {backButtonType === "default" && (
+    <button className="pl-[0.8rem] py-[0.8rem]" onClick={goPrev}>
+      <img src="/icons/icon-back.svg" alt="뒤로 가기" width="24" height="24" />
+    </button>
+  );
+};
+
+const HomeButton = () => {
+  return (
+    <Link href="/">
+      <button className="py-[0.8rem]">
         <img
-          src="/icons/icon-back.svg"
-          alt="뒤로 가기"
+          src="/icons/icon-home.svg"
+          alt="홈으로 가기"
           width="24"
           height="24"
         />
-      )}
-      {backButtonType === "home" && (
-        <img
-          src="/icons/icon-back-home.svg"
-          alt="홈으로 가기"
-          width="44"
-          height="24"
-        />
-      )}
-    </button>
+      </button>
+    </Link>
   );
 };
