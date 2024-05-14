408ae82c99a712d60dd06e9f5cb770f6b7ed9aa3
gxxrxn <rbfks1221@naver.com>
2024-05-15 02:50:21 +0900

    [#14] chore: text-[#ffffff] -> text-white로 수정

--- src/components/common/BottomActionButton.tsx
+++ src/components/common/BottomActionButton.tsx
@@ -7,7 +7,7 @@ const BottomActionButton = ({
   return (
     <div className="fixed inset-x-0 bottom-0 p-[2rem] pt-[0.5rem] bg-white shadow-white-top">
       <button
-        className="w-full h-[4.8rem] font-bold rounded-[1rem] bg-primary text-[#ffffff] hover:bg-primary/90 disabled:bg-shadow-light"
+        className="w-full h-[4.8rem] font-bold rounded-[1rem] bg-primary text-white hover:bg-primary/90 disabled:bg-shadow-light"
         {...props}
       >
         {children}
