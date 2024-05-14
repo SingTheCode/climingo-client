068bae6d846d5e78c34361d3d5db9db30fb59b25
gxxrxn <rbfks1221@naver.com>
2024-05-15 01:41:48 +0900

    [#14] feat: 암장 선택 컴포넌트 마크업

---
+++ src/components/record/create/selectors/PlaceSelector.tsx
@@ -0,0 +1,38 @@
+import { ClimbingPlace } from "@/types/common";
+
+import { Placeholder } from "@/components/record/create/common";
+import ClearButton from "@/components/record/create/ClearButton";
+
+type PlaceSelectorProps = {
+  place?: ClimbingPlace;
+  onClick?: () => void;
+  onClear?: () => void;
+};
+
+const PlaceSelector = ({ place, onClick, onClear }: PlaceSelectorProps) => {
+  return (
+    <div
+      className="h-[4.4rem] pl-[1.4rem] pr-[1.2rem] py-[1rem] w-full bg-shadow-lighter rounded-[0.8rem] flex justify-between items-center cursor-pointer hover:bg-shadow-light/30"
+      onClick={onClick}
+    >
+      {!place ? (
+        <>
+          <Placeholder text="암장을 검색해보세요" />
+          <img
+            src="/icons/icon-arrow-grey.svg"
+            alt="선택"
+            width="20"
+            height="20"
+          />
+        </>
+      ) : (
+        <>
+          <p className="text-sm text-shadow-darkest">{place.name}</p>
+          <ClearButton onClick={onClear} />
+        </>
+      )}
+    </div>
+  );
+};
+
+export default PlaceSelector;
