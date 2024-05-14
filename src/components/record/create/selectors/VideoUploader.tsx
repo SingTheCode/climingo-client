f8c7617bc79d61799075fcaacb53700d772aa01b
gxxrxn <rbfks1221@naver.com>
2024-05-15 01:48:14 +0900

    [#14] feat: 비디오 업로드 컴포넌트 마크업

---
+++ src/components/record/create/selectors/VideoUploader.tsx
@@ -0,0 +1,54 @@
+"use client";
+
+import { useState } from "react";
+
+import ClearButton from "@/components/record/create/ClearButton";
+
+const VideoUploader = () => {
+  const [file, setFile] = useState<string>();
+
+  const uploadFile = () => {};
+
+  const clearVideo = () => {
+    setFile(undefined);
+  };
+
+  return (
+    <div className="relative w-[12rem] h-[16rem] rounded-[0.8rem] bg-shadow-lighter hover:bg-shadow-light/30">
+      <label
+        className={`w-full h-full flex justify-center items-center cursor-pointer`}
+        htmlFor="record-video"
+      >
+        {file ? (
+          <video
+            src={file}
+            controls
+            className="w-full h-full rounded-[0.8rem]"
+          />
+        ) : (
+          <img
+            src="/icons/icon-photo.svg"
+            alt="add-photo-icon"
+            width="20"
+            height="20"
+          />
+        )}
+        <input
+          id="record-video"
+          type="file"
+          accept="video/*"
+          onChange={uploadFile}
+          hidden
+        />
+      </label>
+      {file && (
+        <ClearButton
+          className="absolute top-0 right-0 p-2"
+          onClick={clearVideo}
+        />
+      )}
+    </div>
+  );
+};
+
+export default VideoUploader;
--- src/components/record/create/selectors/index.ts
+++ src/components/record/create/selectors/index.ts
@@ -1,2 +1,3 @@
 export { default as PlaceSelector } from "./PlaceSelector";
 export { default as LevelSelector } from "./LevelSelector";
+export { default as VideoUploader } from "./VideoUploader";
