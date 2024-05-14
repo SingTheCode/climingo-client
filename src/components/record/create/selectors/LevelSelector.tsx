864f6d82cad3e30e32f6bd767eec232b989547d3
gxxrxn <rbfks1221@naver.com>
2024-05-15 01:43:18 +0900

    [#14] feat: 난이도 선택 컴포넌트 마크업

---
+++ src/components/record/create/selectors/LevelSelector.tsx
@@ -0,0 +1,45 @@
+import { Level } from "@/types/common";
+
+import { BOULDER_LEVELS } from "@/constants/level";
+
+import { Placeholder } from "@/components/record/create/common";
+import LevelRadioItem from "./LevelRadioItem";
+
+// FIXME: useQuery로 작성 후 대체
+const useLevelQuery = (_placeId?: number) => {
+  return {
+    data: BOULDER_LEVELS,
+  };
+};
+
+const LevelSelector = ({
+  placeId,
+  onChange,
+}: {
+  placeId?: number;
+  onChange?: (level: Level) => void;
+}) => {
+  const { data } = useLevelQuery(placeId);
+
+  const handleItemClick = (level: Level) => {
+    onChange?.(level);
+  };
+
+  if (!placeId) {
+    return <Placeholder text="암장을 먼저 선택해주세요" />;
+  }
+
+  return (
+    <ul className="flex flex-wrap gap-[1rem]">
+      {data.map((level) => (
+        <LevelRadioItem
+          key={level.id}
+          level={level}
+          onClick={handleItemClick}
+        />
+      ))}
+    </ul>
+  );
+};
+
+export default LevelSelector;
--- src/components/record/create/selectors/index.ts
+++ src/components/record/create/selectors/index.ts
@@ -1,1 +1,2 @@
 export { default as PlaceSelector } from "./PlaceSelector";
+export { default as LevelSelector } from "./LevelSelector";
