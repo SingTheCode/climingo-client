1a7d638e13781d64f2c7208d3071677c52a4ea45
gxxrxn <rbfks1221@naver.com>
2024-05-15 18:44:35 +0900

    [#53] fix: 로그인, 회원가입 페이지 Layout 컴포넌트 적용

--- src/app/signUp/page.tsx
+++ src/app/signUp/page.tsx
@@ -1,5 +1,12 @@
+import Layout from "@/components/common/Layout";
+import NavigationHeader from "@/components/common/NavigationHeader";
 import SignUp from "@/app/signUp/SignUp";
 
 export default function Page() {
-  return <SignUp />;
+  return (
+    <Layout containHeader>
+      <NavigationHeader />
+      <SignUp />
+    </Layout>
+  );
 }
