edcf345bb5e6813e3f0b0867e00847e7bcab95e1
gxxrxn <rbfks1221@naver.com>
2024-06-23 03:12:17 +0900

    [#8] refactor: /myProfile/detail 로 경로 수정

--- src/app/myProfile/detail/page.tsx
+++ src/app/myProfile/detail/page.tsx
@@ -1,14 +1,14 @@
 import Layout from "@/components/common/Layout";
 import AuthRequired from "@/components/common/AuthRequired";
 import NavigationHeader from "@/components/common/NavigationHeader";
-import MyProfileInfo from "@/components/profile/MyProfileInfo";
+import MyProfileDetail from "@/components/profile/MyProfileDetail";
 
-export default function MyProfileInfoPage() {
+export default function MyProfileDetailPage() {
   return (
     <Layout containHeader>
       <NavigationHeader pageTitle="내 정보" hideHomeButton />
       <AuthRequired>
-        <MyProfileInfo />
+        <MyProfileDetail />
       </AuthRequired>
     </Layout>
   );
