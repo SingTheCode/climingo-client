90a5374316ada61f246fbc94ed913c905e05baad
gxxrxn <rbfks1221@naver.com>
2024-07-11 23:17:01 +0900

    [#89] chore: createObserver useInView hook 안으로 이동

--- src/hooks/useInView.ts
+++ src/hooks/useInView.ts
@@ -1,10 +1,9 @@
 import { useEffect, useRef, useState } from "react";
 
-import {
-  type ObserverInstanceCallback,
-  createObserver,
-} from "@/utils/observer";
-
+type ObserverInstanceCallback = (
+  inView: boolean,
+  entry: IntersectionObserverEntry
+) => void;
 interface InVeiwOptions extends IntersectionObserverInit {
   initialInView?: boolean;
   onChange?: ObserverInstanceCallback;
@@ -64,3 +63,29 @@ const useInView = ({
 };
 
 export default useInView;
+
+/*
+ * IntersectionObserver 인스턴스를 생성하고, unobserve 함수를 반환합니다.
+ */
+const createObserver = (
+  element: Element,
+  callback: ObserverInstanceCallback,
+  options: IntersectionObserverInit = {}
+) => {
+  const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+      const inView = entry.isIntersecting;
+      callback(inView, entry);
+    });
+  }, options);
+
+  observer.observe(element);
+
+  /*
+   * unobserve
+   */
+  return () => {
+    observer.unobserve(element);
+    observer.disconnect();
+  };
+};
