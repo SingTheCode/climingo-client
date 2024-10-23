b95d070723356d8728ff11cd83b6a40361d3aa52
gxxrxn <rbfks1221@naver.com>
2024-10-23 21:09:09 +0900

    [#110] fix: oauth 페이지 suspense로 래핑

--- src/app/oauth/page.tsx
+++ src/app/oauth/page.tsx
@@ -1,5 +1,11 @@
+import { Suspense } from "react";
+
 import OAuth from "@/components/auth/OAuth";
 
 export default function Page() {
-  return <OAuth />;
+  return (
+    <Suspense>
+      <OAuth />
+    </Suspense>
+  );
 }
