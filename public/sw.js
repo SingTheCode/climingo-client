ded9ab1899c18fbd71cf109458429c35517ac825
gxxrxn <rbfks1221@naver.com>
2024-11-20 21:27:47 +0900

    [#110] chore: console 주석 제거

--- public/sw.js
+++ public/sw.js
@@ -1,9 +1,9 @@
 self.addEventListener("install", () => {
-  // console.log("service worker installed");
+  // 새로 등록되는 서비스 워커를 바로 active 상태로 만들기 위해 호출
   self.skipWaiting();
 });
 
 self.addEventListener("activate", () => {
-  // console.log("service worker activated");
+  // 새롭게 등록된 서비스 워커가 현채 페이지를 즉시 제어
   self.clients.claim();
 });
