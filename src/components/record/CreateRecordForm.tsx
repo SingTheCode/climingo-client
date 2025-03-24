e908cfd96ffaa06000e35ae8d0fc19dbedd7d97e
spiderq2697 <spiderq10@gmail.com>
2025-03-25 00:08:33 +0900

    [#152] feat: 유의사항 문구 영역 구현

--- src/components/record/CreateRecordForm.tsx
+++ src/components/record/CreateRecordForm.tsx
@@ -12,6 +12,7 @@ import {
 import BottomActionButton from "@/components/common/BottomActionButton";
 import SelectPlaceWithLevel from "@/components/record/SelectPlaceWithLevel";
 import UploadVideo from "@/components/record/UploadVideo";
+import Caution from "@/components/record/Caution";
 
 type FormValues = {
   place?: number;
@@ -78,6 +79,27 @@ const CreateRecordForm = () => {
       <UploadVideo
         validate={(valid) => setValidation({ ...validation, video: valid })}
       />
+      <Caution>
+        <Caution.Title>게시글 작성 유의사항</Caution.Title>
+        <Caution.SubTitle>
+          작성된 게시글은 아래와 같은 기준 하에 관리자에 의해 통보 없이 임의
+          삭제될 수 있습니다.
+        </Caution.SubTitle>
+        <Caution.List>
+          <Caution.Li>
+            개인정보가 포함될 경우 (이름, 전화번호, 주민번호, 아이디 등)
+          </Caution.Li>
+          <Caution.Li>
+            타인의 게시글, 캡처 이미지 등을 무단으로 사용하여 저작권 및 초상권을
+            침해하는 경우
+          </Caution.Li>
+          <Caution.Li>해당 서비스와 관련된 내용이 아닌 경우</Caution.Li>
+          <Caution.Li>욕설, 비방 및 도배글, 하위글인 경우</Caution.Li>
+          <Caution.Li>광고, 홍보, 판매 등의 내용인 경우</Caution.Li>
+          <Caution.Li>부적절한 이미지로 판단될 경우</Caution.Li>
+          <Caution.Li>기타 법령에 위반한 경우</Caution.Li>
+        </Caution.List>
+      </Caution>
       <BottomActionButton type="submit" disabled={!isValid || isPending}>
         {/** TODO: Loading 컴포넌트로 대체 */}
         {isPending ? "기록 중..." : "완료"}
