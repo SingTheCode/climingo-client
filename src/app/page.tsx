6c214d50310bf49839d1b8f4dc056fdbba8d0d01
SingTheCode <spiderq10@gmail.com>
2026-04-07 15:55:44 +0900

    fix: 홈 페이지 prerender 비활성화로 Vercel 배포 실패 해결

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,8 @@
 "use client";
 
+// 빌드 시 prerender에서 API fetch 실패 방지 (api.climingo.xyz DNS 미해석)
+export const dynamic = "force-dynamic";
+
 import { Transition } from "@headlessui/react";
 import Image from "next/image";
 import Link from "next/link";
